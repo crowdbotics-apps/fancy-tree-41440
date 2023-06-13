ed1ecf60d45e652052e976ddc2063a68b64f11d1
Crowdbotics <team@crowdbotics.com>
2023-06-13 03:26:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-tree-41440",
+  url: "https://fancy-tree-41440.botics.co",
+  api: "https://fancy-tree-41440.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
